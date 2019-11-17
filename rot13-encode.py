78160084e374924963a100e84ab840b0ea393160
techdox <nicholas.lee.wilkinson@gmail.com>
2019-11-18 09:45:44 +1300

    Changed var name

--- rot13-encode.py
+++ rot13-encode.py
@@ -4,8 +4,8 @@ import codecs
 def rotCHange():
     rinput = input("Enter in your phrase to convert.")
     enc = codecs.getencoder("rot-13")
-    os = enc(rinput)[0]
-    print(os)
+    rot = enc(rinput)[0]
+    print(rot)
 
 if __name__ == "__main__":
     rotCHange()    
